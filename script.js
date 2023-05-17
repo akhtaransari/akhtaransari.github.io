fcc3a9136fa896429117a495350f5c8891ff7547
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-05-18 00:06:53 +0530

    D2 all solved

--- script.js
+++ script.js
@@ -35,18 +35,12 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Dreamer"],
+    strings: ["Back-End Developer", "latte-Artist", "Chef"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Back-End Developer", "Dreamer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
 
   // owl carousel script
   $(".carousel").owlCarousel({
