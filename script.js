1f0e92e4099e42a4cfad5a1c2593f7ff35ed09ea
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-05-30 18:51:56 +0530

    part 1

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Dreamer"],
+    strings: ["Back-End Developer", "Latte Art Artist", "Fitness Trainer","Front-End Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
