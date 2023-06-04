0d6c302f32cdedc5d8ad2354cc9ee86d8fd8b80d
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-06-04 17:13:21 +0530

    My Portfolio With Responsiveness added and Final Update

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ $(document).ready(function () {
     $("html").css("scrollBehavior", "auto");
   });
 
-  $(".nav-menu .menu li a").click(function () {
+  $("#nav-menu .menu li a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
   // toggle menu/nav-menu script
   $(".menu-btn").click(function () {
-    $(".nav-menu .menu").toggleClass("active");
+    $("#nav-menu .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
 
